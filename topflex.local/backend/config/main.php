7290dff610f9951c38107146640f97ac4328cc2e
Zub <zub.s@novaposhta.ua>
2017-05-19 16:08:16 +0300

    Initial commit

---
+++ topflex.local/backend/config/main.php
@@ -0,0 +1,74 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+use \yii\web\Request;
+$adminUrl = str_replace('/backend/web', '/admin', (new Request)->getBaseUrl());
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-backend',
+            'baseUrl' => '/admin',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the backend
+            'name' => 'advanced-backend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+/*
+        'urlManager' => [
+//            'baseUrl' => $adminUrl,
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+//                '' => 'site/index',
+//                '<controller:\w+>/<action:\w+>/' => '<controller>/<action>',
+            ],
+        ],*/
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            //'suffix' => '.html',
+            'rules' => [
+                '' => 'site/index',
+
+
+                '<action>'=>'site/<action>',
+            ],
+        ],
+        'assetManager' => [
+            'basePath' => '@webroot/assets',
+            'baseUrl' => '@web/assets'
+        ],
+//        'request' => [
+//            'baseUrl' => '/admin'
+//        ]
+
+    ],
+    'params' => $params,
+];
